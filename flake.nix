27eab8d0f363abcce8e845707246c1282763e39e
LucasPC-hub <lucas.pc99@hotmail.com>
2026-03-14 08:56:49 -0300

    Auto-commit  M flake.nix

--- flake.nix
+++ flake.nix
@@ -58,6 +58,7 @@
                   doc = final.emptyDirectory;
                 };
               });
+              quickshell = inputs.quickshell.packages.${system}.default;
             })
           ];
         }
