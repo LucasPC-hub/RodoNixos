f478a8c836ce19faf76ae96e599ed65ef963a3ea
LucasPC-hub <lucas.pc99@hotmail.com>
2026-03-17 23:47:23 -0300

    Auto-commit  M modules/desktop/stylix.nix

--- modules/desktop/stylix.nix
+++ modules/desktop/stylix.nix
@@ -2,12 +2,12 @@
 
 {
   stylix.enable = true;
-  stylix.autoEnable = true;
+  stylix.autoEnable = false;
   stylix.base16Scheme = toString (builtins.path {
     path = ../../assets/themes/base-16/oled-lavender.yaml;
   });
   stylix.enableReleaseChecks = false;
 
-  # Desabilitar targets que conflitam com configs manuais
-  stylix.targets.gnome.enable = false;
+  # Só o console/TTY
+  stylix.targets.console.enable = true;
 }
