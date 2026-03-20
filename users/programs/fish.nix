e5b31aeb28e1e327980ad09b664cb378e0b4a056
LucasPC-hub <lucas.pc99@hotmail.com>
2026-03-20 17:02:37 -0300

    Auto-commit  M users/programs/fish.nix

--- users/programs/fish.nix
+++ users/programs/fish.nix
@@ -55,23 +55,69 @@ $file_status"
       set -g fish_greeting ""
 
       #============================================
-      # Cores
+      # Catppuccin Mocha Colors
       #============================================
+      set -g catppuccin_rosewater "f5e0dc"
+      set -g catppuccin_flamingo "f2cdcd"
+      set -g catppuccin_pink "f5c2e7"
+      set -g catppuccin_mauve "cba6f7"
+      set -g catppuccin_red "f38ba8"
+      set -g catppuccin_maroon "eba0ac"
+      set -g catppuccin_peach "fab387"
+      set -g catppuccin_yellow "f9e2af"
+      set -g catppuccin_green "a6e3a1"
+      set -g catppuccin_teal "94e2d5"
+      set -g catppuccin_sky "89dceb"
+      set -g catppuccin_sapphire "74c7ec"
+      set -g catppuccin_blue "89b4fa"
+      set -g catppuccin_lavender "b4befe"
+      set -g catppuccin_text "cdd6f4"
+      set -g catppuccin_subtext1 "bac2de"
+      set -g catppuccin_subtext0 "a6adc8"
+      set -g catppuccin_overlay2 "9399b2"
+      set -g catppuccin_overlay1 "7f849c"
+      set -g catppuccin_overlay0 "6c7086"
+      set -g catppuccin_surface2 "585b70"
+      set -g catppuccin_surface1 "45475a"
+      set -g catppuccin_surface0 "313244"
+      set -g catppuccin_base "1e1e2e"
+      set -g catppuccin_mantle "181825"
+      set -g catppuccin_crust "11111b"
+
+      # Fish colors - Catppuccin Mocha
+      set -g fish_color_normal $catppuccin_text
+      set -g fish_color_command $catppuccin_blue
+      set -g fish_color_keyword $catppuccin_mauve
+      set -g fish_color_quote $catppuccin_green
+      set -g fish_color_redirection $catppuccin_pink
+      set -g fish_color_end $catppuccin_peach
+      set -g fish_color_error $catppuccin_red
+      set -g fish_color_param $catppuccin_lavender
+      set -g fish_color_comment $catppuccin_overlay0
+      set -g fish_color_selection --background=$catppuccin_surface0
+      set -g fish_color_search_match --background=$catppuccin_surface0
+      set -g fish_color_operator $catppuccin_sky
+      set -g fish_color_escape $catppuccin_pink
+      set -g fish_color_autosuggestion $catppuccin_overlay0
+      set -g fish_pager_color_progress $catppuccin_overlay0
+      set -g fish_pager_color_prefix $catppuccin_blue
+      set -g fish_pager_color_completion $catppuccin_text
+      set -g fish_pager_color_description $catppuccin_overlay0
+
       set -gx CLICOLOR 1
       set -gx LS_COLORS 'di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'
 
       #============================================
-      # FZF
+      # FZF - Catppuccin Mocha
       #============================================
       set -gx FZF_DEFAULT_OPTS "
         --height 40%
         --layout=reverse
         --border
         --inline-info
-        --color=fg:#c0caf5,bg:#1a1b26,hl:#7aa2f7
-        --color=fg+:#c0caf5,bg+:#292e42,hl+:#7dcfff
-        --color=info:#7aa2f7,prompt:#7dcfff,pointer:#7dcfff
-        --color=marker:#9ece6a,spinner:#9ece6a,header:#9ece6a
+        --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8
+        --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc
+        --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8
       "
       set -gx FZF_CTRL_T_OPTS "--preview 'bat --color=always --style=numbers --line-range=:500 {}' --preview-window right:50%:wrap"
       set -gx FZF_ALT_C_OPTS "--preview 'eza --tree --level=2 --color=always {} 2>/dev/null' --preview-window right:50%"
@@ -83,13 +129,25 @@ $file_status"
       carapace _carapace fish | source
 
       #============================================
-      # Atuin
+      # Atuin (com fzf)
       #============================================
       set -gx ATUIN_NOBIND true
       atuin init fish | source
-      bind \cr _atuin_search
-      bind \e\[A _atuin_search
-      bind \eOA _atuin_search
+
+      # Busca histórico do fish via fzf
+      function _fish_fzf_history
+        set -l output (history | fzf --height 40% --layout=reverse --border --query (commandline -b))
+        if test -n "$output"
+          commandline -r $output
+        end
+        commandline -f repaint
+      end
+
+      bind \cr _fish_fzf_history
+      bind \t _fish_fzf_history
+
+      # Seta pra cima: abre atuin com fzf
+      bind up _fish_fzf_history
 
       #============================================
       # Desabilita kitty keyboard protocol
@@ -128,5 +186,6 @@ $file_status"
 
   home.packages = with pkgs; [
     carapace
+    fishPlugins.fzf-fish
   ];
 }
